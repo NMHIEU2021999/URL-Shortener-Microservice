6ee4452bdae3274de269958b435f6fa4768166d9
Glitch (difficult-rune-sunflower) <none>
2020-05-27 02:14:58 +0000

    🌈🎌 Updated with Glitch

--- server.js
+++ server.js
@@ -3,11 +3,11 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+var dns = require('dns');
 var cors = require('cors');
-
+var crypto = require('crypto');
 var app = express();
-
+let tempDB = [];
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
@@ -30,10 +30,46 @@ app.get('/', function(req, res){
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
-app.get("/api/shorturl/new-:origin_url", (req,res)=>{
-  var originURL = req.params.origin_url;
-  
+app.post("/api/shorturl/new", (req,res)=>{
+  var originURL = req.query.url;
+  dns.lookup(originURL, (err)=>{
+    if(err){
+      res.json({
+        error: "invalid URL"
+      });
+      return;
+    }else{
+      var sha256 = crypto.createHash('sha256').update(originURL, 'utf8').digest('base64');
+      tempDB.push({
+        original_url : originURL,
+        short_url : sha256.substring(0,6)
+      });
+      res.json({
+        original_url : originURL,
+        short_url : sha256.substring(0,6)
+      });
+    }
+  })
 });
+app.get("/api/shorturl/:shorturl", (req,res)=>{
+  var longURL;
+  var found = false;
+  var shortURL = req.params.shorturl;
+  tempDB.forEach((element)=>{
+    if(element.short_url === shortURL){
+      found = true;
+      longURL = element.original_url;
+    }
+  });
+  if(found){
+    res.redirect(longURL);
+    return;
+  }else{
+    res.json({
+        error: "Not Found URL"
+    });
+  }
+})
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
